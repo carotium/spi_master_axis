86dc7f9599a0ee9f1b699f74a72300dbc624254d
carotium <coolboy9987@gmail.com>
2025-09-19 10:20:31 +0000

    Added output netlist.

--- impl/vivado/design.tcl
+++ impl/vivado/design.tcl
@@ -1,8 +1,10 @@
+# Run 'vivado -mode batch -source design.tcl' 
+
 # Define input and output directory area
 
-set script_path [ file dirname [ file normalize [ info script] ] ]
-set project_root_dir $script_path/../../.
-set source_dir $project_root_dir/.
+set script_path         [ file dirname [ file normalize [ info script] ] ]
+set project_root_dir    $script_path/../../.
+set source_dir          $project_root_dir/.
 
 set_part xc7z020clg484-1
 
@@ -11,11 +13,15 @@ file mkdir $output_dir
 
 # Setup design sources and constraints
 
-read_vhdl [ glob $source_dir/spi_master_axis.vhd]
-read_xdc $script_path/zedboard.xdc
+read_vhdl   [ glob $source_dir/spi_master_axis.vhd]
+read_xdc    $script_path/zedboard.xdc
 
 # Run synthesis
-synth_design -top spi_master_axis_top
-write_checkpoint -force $output_dir/post_synth.dcp
-report_timing_summary -file $output_dir/post_synth_timing_summary.rpt
-report_utilization -file $output_dir/post_synth_util.rpt
+synth_design            -top    spi_master_axis
+write_checkpoint        -force  $output_dir/post_synth
+report_timing_summary   -file   $output_dir/post_synth_timing_summary.rpt
+report_power            -file   $output_dir/post_synth_power.rpt
+report_utilization      -file   $output_dir/post_synth_util.rpt
+
+write_vhdl  -force  $output_dir/impl_netlist.vhdl
+write_edif  -force  $output_dir/impl_netlist.edif
