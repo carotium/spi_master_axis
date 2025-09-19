d0d53f002f109ac21b88a7aea92c71c9f1f8c0f4
carotium <coolboy9987@gmail.com>
2025-09-19 08:46:38 +0000

    Added design tcl script
    Adds the spi_master_axis.vhd and tries to synthesize.

---
+++ impl/vivado/design.tcl
@@ -0,0 +1,19 @@
+# Define input and output directory area
+
+set script_path [ file dirname [ file normalize [ info script] ] ]
+set project_root_dir $script_path/../../.
+set source_dir $project_root_dir/.
+
+set output_dir $script_path/output/.
+file mkdir $output_dir
+
+# Setup design sources and constraints
+
+read_vhdl [ glob $source_dir/spi_master_axis.vhd]
+read_xdc $script_path/zedboard.xdc
+
+# Run synthesis
+synth_design -top spi_master_axis_top
+write_checkpoint -force $output_dir/post_synth.dcp
+report_timing_summary -file $output_dir/post_synth_timing_summary.rpt
+report_utilization -file $output_dir/post_synth_util.rpt
